a221ab52ea1f4897091ace227a4f4a6f008a7583
tavadmin <tavadmin@codi-priyanka.internal.tavisca.com>
2020-07-21 18:08:17 +0530

    Updates

---
+++ precache-manifest.e14c27540741a13f4ec2ceb37e5d9a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6ded9760bcf3819a33fc7aee029f48",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8c635ee425f292f0e493",
+    "url": "/Tic-tac-toe/static/css/main.df14b196.chunk.css"
+  },
+  {
+    "revision": "aa259c850f5fed1b3ec8",
+    "url": "/Tic-tac-toe/static/js/2.5c379eb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-tac-toe/static/js/2.5c379eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c635ee425f292f0e493",
+    "url": "/Tic-tac-toe/static/js/main.a4c3c749.chunk.js"
+  },
+  {
+    "revision": "0e73326d868b0e51b171",
+    "url": "/Tic-tac-toe/static/js/runtime-main.dc4e01bb.js"
+  },
+  {
+    "revision": "8421c7ef85630f0183e1135207afd42b",
+    "url": "/Tic-tac-toe/static/media/giphy.8421c7ef.gif"
+  }
+]);
